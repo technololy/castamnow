5885de6bacfffd01dbf8fedfc1530ca74df162c1
Damilola Shopeyin <kaysho109@gmail.com>
2025-08-29 14:02:37 +0100

    Fix api call using refit.

--- src/core/CastAmNow.Sdk/DefectApi.cs
+++ src/core/CastAmNow.Sdk/DefectApi.cs
@@ -2,12 +2,20 @@
 using CastAmNow.Sdk.Abstractions;
 using CastAmNow.Sdk.Implementations;
 using Refit;
+using System.Text.Json;
 
 namespace CastAmNow.Sdk
 {
     public class DefectApi(HttpClient defectClient, BlobContainerClient? blobServiceClient = default) : IDefectApi
     {
-        public IDefectService DefectService => RestService.For<IDefectService>(defectClient);
+        private readonly RefitSettings refitSettings = new()
+        {
+            ContentSerializer = new SystemTextJsonContentSerializer(new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            })
+        };
+        public IDefectService DefectService => RestService.For<IDefectService>(defectClient,refitSettings);
 
         public IStorageUploadService StorageUploadService => new MicrosoftStorageUploadService(blobServiceClient);
     }
