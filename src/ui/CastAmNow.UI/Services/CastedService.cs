aa142fdf187cb500b1b7ad2a8fd9a91c632c7d86
knowmynetwork <loladeking@hotmail.com>
2025-08-29 09:52:36 +0100

    Refactor defect handling and add logging to CastedService
    Updated defect-related DTO naming for clarity and consistency. Introduced logging in `CastedService` to capture API responses and relevant details. Extracted `ICastedService` into its own file for better service interface management.

--- src/ui/CastAmNow.UI/Services/CastedService.cs
+++ src/ui/CastAmNow.UI/Services/CastedService.cs
@@ -1,26 +1,30 @@
 using CastAmNow.Core.Dtos.Defect;
 using CastAmNow.Sdk;
+using Microsoft.Extensions.Logging;
 
 namespace CastAmNow.UI.Services;
 
-public class CastedService(IDefectApi api) : ICastedService
+public class CastedService(
+    IDefectApi api,
+    ILogger<CastedService> logger) : ICastedService
 {
     public async Task<DefectDto> SubmitCastedDefectsAsync(CreateDefectDto createDefectDto)
     {
         var response = await api.DefectService.CreateDefectAsync(createDefectDto);
+        logger.LogInformation("response from api:{statusCode} is {message}. the route is {route}",
+            response.StatusCode,
+            response.Content?.Message,
+            response.RequestMessage?.RequestUri?.ToString());
         return response.Content?.Data ?? new DefectDto();
     }
 
     public async Task<IEnumerable<DefectDto>> GetCastedDefectsAsync()
     {
-        var response2 = await api.DefectService.GetDefectAsync();
-        return response2.Content?.Data ?? [];
+        var response = await api.DefectService.GetDefectAsync();
+        logger.LogInformation("response from api:{statusCode} is {message}. the route is {route}",
+            response.StatusCode,
+            response.Content?.Message,
+            response.RequestMessage?.RequestUri?.ToString());
+        return response.Content?.Data ?? [];
     }
-}
-
-public interface ICastedService
-{
-    public Task<DefectDto> SubmitCastedDefectsAsync(CreateDefectDto createDefectDto);
-
-    public Task<IEnumerable<DefectDto>> GetCastedDefectsAsync();
 }
