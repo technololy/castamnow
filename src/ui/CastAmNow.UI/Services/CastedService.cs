0b0abfee3215c2c68ef5f6d4c3a30ec30adc5ad0
knowmynetwork <loladeking@hotmail.com>
2025-09-01 01:03:37 +0100

    Refactor code structure for improved readability and maintainability

--- src/ui/CastAmNow.UI/Services/CastedService.cs
+++ src/ui/CastAmNow.UI/Services/CastedService.cs
@@ -1,11 +1,13 @@
 using CastAmNow.Core.Dtos.Defect;
 using CastAmNow.Sdk;
+using CastAmNow.Sdk.Abstractions;
 using Microsoft.Extensions.Logging;
 
 namespace CastAmNow.UI.Services;
 
 public class CastedService(
     IDefectApi api,
+    IDefectService defectService,
     ILogger<CastedService> logger) : ICastedService
 {
     public async Task<DefectDto?> SubmitCastedDefectsAsync(CreateDefectDto createDefectDto)
@@ -21,6 +23,15 @@ public class CastedService(
     public async Task<IEnumerable<DefectDto>> GetCastedDefectsAsync()
     {
         var response = await api.DefectService.GetDefectAsync();
+        logger.LogInformation("response from api:{statusCode} is {message}. the route is {route}",
+            response.StatusCode,
+            response.Content?.Message,
+            response.RequestMessage?.RequestUri?.ToString());
+        return response.Content?.Data ?? [];
+    }
+    public async Task<IEnumerable<DefectDto>> SearchCastedDefectsAsync(string searchTerm)
+    {
+        var response = await defectService.GetDefectAsync();
         logger.LogInformation("response from api:{statusCode} is {message}. the route is {route}",
             response.StatusCode,
             response.Content?.Message,
--- src/ui/CastAmNow.UI/Services/ICastedService.cs
+++ src/ui/CastAmNow.UI/Services/ICastedService.cs
@@ -4,6 +4,8 @@ namespace CastAmNow.UI.Services;
 
 public interface ICastedService
 {
+    public Task<IEnumerable<DefectDto>> SearchCastedDefectsAsync(string searchTerm);
+
     public Task<DefectDto?> SubmitCastedDefectsAsync(CreateDefectDto createDefectDto);
 
     public Task<IEnumerable<DefectDto>> GetCastedDefectsAsync();
